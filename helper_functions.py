eefcdf6007f4b4020385b7752660acb206881868
Omer Ben Salmon <o_bensalmon@apple.com>
2023-09-11 09:49:21 +0300

    added prep pixels func

--- helper_functions.py
+++ helper_functions.py
@@ -1,17 +1,18 @@
 from tensorflow.keras.datasets import mnist
 from matplotlib import pyplot as plt
 
+
 # load train and test dataset
 def load_dataset():
- # load dataset
- (trainX, trainY), (testX, testY) = mnist.load_data()
- # reshape dataset to have a single channel
- trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
- testX = testX.reshape((testX.shape[0], 28, 28, 1))
- # one hot encode target values
- trainY = to_categorical(trainY)
- testY = to_categorical(testY)
- return trainX, trainY, testX, testY
+    # load dataset
+    (train_x, train_y), (test_x, test_y) = mnist.load_data()
+    # reshape dataset to have a single channel
+    train_x = train_x.reshape((train_x.shape[0], 28, 28, 1))
+    test_x = test_x.reshape((test_x.shape[0], 28, 28, 1))
+    # one hot encode target values
+    train_y = to_categorical(train_y)
+    test_y = to_categorical(test_y)
+    return train_x, train_y, test_x, test_y
 
 
 def show_data():
@@ -27,3 +28,14 @@ def show_data():
         # plot raw pixel data
         plt.imshow(trainX[i], cmap=plt.get_cmap('gray'))
     # show the figure
+
+
+def prep_pixels(train, test):
+    # convert from integers to floats
+    train_norm = train.astype('float32')
+    test_norm = test.astype('float32')
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # return normalized images
+    return train_norm, test_norm
